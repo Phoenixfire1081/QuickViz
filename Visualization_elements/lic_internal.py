8628f966a0085fd7c03c265452586c148ad29868
Abhishek Paraswarar <abi.abhishek7@gmail.com>
2025-10-06 15:39:57 +0200

    Fixed LIC issues..

--- Visualization_elements/lic_internal.py
+++ Visualization_elements/lic_internal.py
@@ -5,53 +5,53 @@
 import numpy as np
 from numba import jit
 
-@jit(nopython = True, cache = True)
-def void _advance(vx, vy, x, y, fx, fy, w, h):
-    if vx>=0:
-        tx = (1-fx[0])/vx
+
+@jit(nopython=True, cache=True)
+def _advance(vx, vy, x, y, fx, fy, w, h):
+    if vx >= 0:
+        tx = (1 - fx) / vx
     else:
-        tx = -fx[0]/vx
-    if vy>=0:
-        ty = (1-fy[0])/vy
+        tx = -fx / vx
+    if vy >= 0:
+        ty = (1 - fy) / vy
     else:
-        ty = -fy[0]/vy
-    if tx<ty:
-        if vx>=0:
-            x[0]+=1
-            fx[0]=0
+        ty = -fy / vy
+    if tx < ty:
+        if vx >= 0:
+            x += 1
+            fx = 0
         else:
-            x[0]-=1
-            fx[0]=1
-        fy[0]+=tx*vy
+            x -= 1
+            fx = 1
+        fy += tx * vy
     else:
-        if vy>=0:
-            y[0]+=1
-            fy[0]=0
+        if vy >= 0:
+            y += 1
+            fy = 0
         else:
-            y[0]-=1
-            fy[0]=1
-        fx[0]+=ty*vx
-    if x[0]>=w:
-        x[0]=w-1 # FIXME: other boundary conditions?
-    if x[0]<0:
-        x[0]=0 # FIXME: other boundary conditions?
-    if y[0]<0:
-        y[0]=0 # FIXME: other boundary conditions?
-    if y[0]>=h:
-        y[0]=h-1 # FIXME: other boundary conditions?
+            y -= 1
+            fy = 1
+        fx += ty * vx
+    if x >= w:
+        x = w - 1  # FIXME: other boundary conditions?
+    if x < 0:
+        x = 0  # FIXME: other boundary conditions?
+    if y < 0:
+        y = 0  # FIXME: other boundary conditions?
+    if y >= h:
+        y = h - 1  # FIXME: other boundary conditions?
+
+    return fx, fy, x, y
 
 
-#np.ndarray[float, ndim=2] 
-@jit(nopython = True, cache = True)
+# np.ndarray[float, ndim=2]
+@jit(nopython=True, cache=True)
 def line_integral_convolution(vectors, texture, kernel):
-
     h = vectors.shape[0]
     w = vectors.shape[1]
     t = vectors.shape[2]
     kernellen = kernel.shape[0]
-    if t!=2:
-        raise ValueError("Vectors must have two components (not %d)" % t)
-    result = np.zeros((h,w),dtype=np.float32)
+    result = np.zeros((h, w), dtype=np.float32)
 
     for i in range(h):
         for j in range(w):
@@ -59,28 +59,25 @@ def line_integral_convolution(vectors, texture, kernel):
             y = i
             fx = 0.5
             fy = 0.5
-            
-            k = kernellen//2
-            #print i, j, k, x, y
-            result[i,j] += kernel[k]*texture[x,y]
-            while k<kernellen-1:
-                _advance(vectors[y,x,0],vectors[y,x,1],
-                        x, y, fx, fy, w, h)
-                k+=1
-                #print i, j, k, x, y
-                result[i,j] += kernel[k]*texture[x,y]
+
+            k = kernellen // 2
+            # print i, j, k, x, y
+            result[i, j] += kernel[k] * texture[x, y]
+            while k < kernellen - 1:
+                fx, fy, x, y = _advance(vectors[y, x, 0], vectors[y, x, 1], x, y, fx, fy, w, h)
+                k += 1
+                # print i, j, k, x, y
+                result[i, j] += kernel[k] * texture[x, y]
 
             x = j
             y = i
             fx = 0.5
             fy = 0.5
-            
-            while k>0:
-                _advance(-vectors[y,x,0],-vectors[y,x,1],
-                        x, y, fx, fy, w, h)
-                k-=1
-                #print i, j, k, x, y
-                result[i,j] += kernel[k]*texture[x,y]
-                    
-    return result
 
+            while k > 0:
+                fx, fy, x, y = _advance(-vectors[y, x, 0], -vectors[y, x, 1], x, y, fx, fy, w, h)
+                k -= 1
+                # print i, j, k, x, y
+                result[i, j] += kernel[k] * texture[x, y]
+
+    return result
